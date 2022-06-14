7752f29a34481f27c77325bd41a746adfe00ae5c
yuri1709 <yuriconder184@gmail.com>
2022-06-13 23:46:48 -0300

    tela de cadastro livro

---
+++ Livraria/src/main/java/model/Login.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Gamer
+ */
+public class Login {
+    String Username;
+    String Password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+    public void criarLogin(){
+        
+    }
+    
+    public void validarLogin() {
+        
+    }
+        
+}
